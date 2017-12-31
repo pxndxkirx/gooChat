735ebd69dce39089fbec722bd244c82c147df968
pxndxkirx <pxndxkirx@gmail.com>
2017-12-31 01:35:16 -0400

    mostrar ultimos mensajes y notificacions de leidos terminado

--- js/main.js
+++ js/main.js
@@ -1,95 +1,101 @@
 var databaseService = firebase.database();
-var refCustomerGroups;
-var refCustomerReceivedMessage;
-var refMensagesClientSent;
-var refMessage_group;
+
 var refBusiness_circle;
 var refListUser;
+var refChat;
+
+
 //var json de los datos
-var jsonCustomerGroups;
-var jsonCustomerReceivedMessage;
-var jsonMensagesClientSent;
-var jsonMessage_group;
+
 var jsonBusiness_circle;
 var jsonListUser;
-
+var jsonChat;
 
 
 //variable del id del contacto a enviar;
 var id_contact_send;
 
-
-
+var id;
 
 
 
 function idClient() {
     //doy valor a las variables de los eventos
-    var id = document.getElementById('id');
+    id = document.getElementById('id');
     console.log("entro al idClient " + id.value);
+    
     refListUser=firebase.database().ref().child('list_user');
     refBusiness_circle = firebase.database().ref().child('business_circle').child(id.value);
-    refCustomerGroups = firebase.database().ref().child('customerGroups').child(id.value);
-    refCustomerReceivedMessage = firebase.database().ref().child('customerReceivedMessage').child(id.value);
-    refMensagesClientSent = firebase.database().ref().child('mensagesClientSent').child(id.value);
-    refMessage_group = firebase.database().ref().child('message_group').child(id.value);
+    refChat=firebase.database().ref().child('chat').child(id.value);
+
+
+
     vigilarCambios();
     mostrarContactos();
 }
 
 function vigilarCambios() {
 
-
-            // nota , pensar como vamos a mostrar el chat
-
-
-
-
-
-
-
-
     console.log("entro a vigilar cambios");
-    refCustomerGroups.on("value", snap => {
-        jsonCustomerGroups = JSON.stringify(snap.val(), null, 3);
-        console.log("1");
-        console.log(jsonCustomerGroups);
-    });
-    refCustomerReceivedMessage.on("value", snap => {
-        jsonCustomerReceivedMessage = JSON.stringify(snap.val(), null, 3);
-        console.log("2");
-        console.log(jsonCustomerReceivedMessage);
-        jsonCustomerReceivedMessage=JSON.parse(jsonCustomerReceivedMessage);
-    });
-    refMensagesClientSent.on("value", snap => {
-        jsonMensagesClientSent = JSON.stringify(snap.val(), null, 3);
-        console.log("3");
-        console.log(jsonMensagesClientSent);
-
-        jsonMensagesClientSent=JSON.parse(jsonMensagesClientSent);
-    });
+ 
 
 
 
-    refMessage_group.on("value", snap => {
-        jsonMessage_group = JSON.stringify(snap.val(), null, 3);
-        console.log("4");
-        console.log(jsonMessage_group);
 
-        jsonMessage_group=JSON.parse(jsonMessage_group);
-    });
-
+   //recupero toda la lista de usuarios
     refListUser.on("value",snap=>{
         jsonListUser=JSON.stringify(snap.val(),null,3);
         jsonListUser=JSON.parse(jsonListUser);
     });
+   //recupero los mensajes del usuario
+
+   
+ refChat.on("value",snap=>{
+        jsonChat=JSON.stringify(snap.val(),null,3);
+        console.log(jsonChat);
+        jsonChat=JSON.parse(jsonChat);
+        document.getElementById('viewMessages').innerHTML="";
+        var sinLeer=0;
+        var nombre;
+        var ultimoMensaje;
+        var fecha;
+        var hora;
+        var id_ultimo;
+        for(var key in jsonChat){
+            nombre=nameBussines(key);
+            var jsonTemp=jsonChat[key];
+            for(var k in jsonTemp){
+                ultimoMensaje=jsonTemp[k].message;
+                fecha=jsonTemp[k].date;
+                hora=jsonTemp[k].hours;
+                id_ultimo=jsonTemp[k].id;
+                if(id.value != jsonTemp[k].id){
+                    if(jsonTemp[k].viewed==false){
+                        sinLeer++;
+                    }
+                }
+
+            }
+
+            document.getElementById('infoMessages').innerHTML=sinLeer;
+            document.getElementById('unreadMessage').innerHTML=sinLeer;
+            document.getElementById('viewMessages').innerHTML+="<div class='col-12'><div class='viewMessage' onclick='selectedItem(this)'><div><div class='viewContact'><img src='https://ind.proz.com/zf/images/default_user_512px.png' class='perfil' alt=''><div class='contacDat'><div class='nameContact'><h4>"+nombre+"</h4></div><div class='rigthDat'><h5>"+hora+"<br>"+fecha+"</h5></div></div></div><div class='message'><h5>"+nameBussines(id_ultimo)+": "+ultimoMensaje+"</h5></div></div></div></div>";
 
 
+
+
+
+            console.log("nombre:"+nombre);
+            console.log("ultimo mensaje: "+ultimoMensaje);
+            console.log("fecha y hora: "+fecha+" "+hora);
+        }
+
+    });
 
     //mostrar los circulos o contactos del usuario
     refBusiness_circle.on("value", snap => {
         jsonBusiness_circle = JSON.stringify(snap.val(), null, 3);
-        console.log("5");
+        // console.log("5");
         console.log(jsonBusiness_circle);
         jsonBusiness_circle=JSON.parse(jsonBusiness_circle);
         //mostrando los datos
@@ -97,7 +103,7 @@ function vigilarCambios() {
         document.getElementById("viewMessage").innerHTML=""; 
         var ic=0;
         for(var key in jsonBusiness_circle){
-           console.log("name="+key+" value="+jsonBusiness_circle[key].name_bussines);
+           // console.log("name="+key+" value="+jsonBusiness_circle[key].name_bussines);
             var nombre=jsonBusiness_circle[key].name_bussines;
             var descripcion=jsonBusiness_circle[key].description;
           // document.getElementById("viewMessage").innerHTML+="  <div class='col-12'><div class='viewMessage' ><div><div class='viewContact'><img src='https://ind.proz.com/zf/images/default_user_512px.png' class='perfil' alt=''><div class='contacDat'><div class='nameContact'><h4>"+nombre+"</h4></div><div class='rigthDat'><h5>22:43<br>12/12/12</h5></div></div></div><div class='message'><h5>ultimo mensaje</h5></div></div></div></div>"
@@ -109,10 +115,106 @@ function vigilarCambios() {
 }
 
 
+//esta funcion me proporciona el id del item seleccionado para poder enviarle un mensaje o leer los mensajes seleccionados
+
+function selectedItem(obj){
+    var nameBussines=stripHtmlTags(obj.childNodes[0].childNodes[0].childNodes[1].childNodes[0].childNodes[0]);
+    console.log(nameBussines);
+    console.log(obj);
+    var a=document.getElementsByClassName("viewMessage");
+    console.log(a.length);
+    for(var i=0;i<a.length;i++){
+        a[i].className="viewMessage";
+        console.log("entro");
+    }
+    //resalto el item seleccionado
+    obj.className="viewMessage selectedItem";
+
+    //le asigno el id del item seleccionado
+    id_contact_send=idItem(nameBussines);
+
+    console.log(id_contact_send);
+
+
+
+}
+
+//elimina etiquetas de un objeto
+function stripHtmlTags(elemento) {
+  return elemento.textContent||elemento.innerText;
+}
+
+//funcion que me devuelve el id segun el nombre
+function idItem(name){
+
+    for(var key in jsonListUser){
+
+        if(jsonListUser[key].name_bussines == name){
+          //console.log(key + " "+jsonListUser[key].name_bussines);
+          return key;
+        }
+        //console.log(key);
+    }
+    return console.log("Este usuario no se encuentra en la lista de usuarios");
+}
+
+
+//funcion ke devuelve el nombre segun el id
+
+function nameBussines(id){
+        for(var key in jsonListUser){
+            if( key == id){
+             return jsonListUser[key].name_bussines;
+            }
+        }
+    return "error";
+}
 
 
 
 
+
+
+
+
+
+
+
+
+//esta funcion me mostrara todos los mensajes ke se tubo con un usuario en espesifico
+
+function mostrarMensajes(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//funcion que crea el nuevo id para el nuevo mensaje
+function idNewMessage(id){
+    var f=new Date();
+    return f.getHours()+""+f.getMinutes()+""+f.getSeconds()+""+mls(f.getTime())+""+f.getDate()+""+(f.getMonth()+1)+""+f.getFullYear()+id;
+}
+//funcion que me devuelve los milisegundos de un estring del getTime();
+function mls(b){
+    a=b+"";
+    console.log(a.length);
+    return a.substr(a.length-3,1);
+}
 
 
 
@@ -199,46 +301,5 @@ function mostrarSolicitudes(){
 
     console.log("Mostrando solicitudes de amistad");
 };
-
-
-
-// function extractId()
-
-function selectedItem(obj){
-    var nameBussines=stripHtmlTags(obj.childNodes[0].childNodes[0].childNodes[1].childNodes[0].childNodes[0]);
-    console.log(nameBussines);
-    console.log(obj);
-    var a=document.getElementsByClassName("viewMessage");
-    console.log(a.length);
-    for(var i=0;i<a.length;i++){
-        a[i].className="viewMessage";
-        console.log("entro");
-    }
-    //resalto el item seleccionado
-    obj.className="viewMessage selectedItem";
-
-    //le asigno el id del item seleccionado
-    id_contact_send=idItem(nameBussines);
-    console.log(id_contact_send);
-}
-
-//elimina etiquetas de un objeto
-function stripHtmlTags(elemento) {
-  return elemento.textContent||elemento.innerText;
-}
-
-//funcion que me devuelve el id segun el nombre
-function idItem(name){
-
-    for(var key in jsonListUser){
-
-        if(jsonListUser[key].name_bussines == name){
-          //console.log(key + " "+jsonListUser[key].name_bussines);
-          return key;
-        }
-        //console.log(key);
-    }
-    return console.log("Este usuario no se encuentra en la lista de usuarios");
-}
 
 
